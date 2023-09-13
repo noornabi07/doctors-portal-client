b01ce660c9e972eef6c2c90070388b15212a5577
noornabi07 <noornabiprogram07@gmail.com>
2023-09-13 12:03:03 +0600

    doctors portal navbar and banner section done

---
+++ src/Routes/components/Home/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import bannerGroup from '../../../../assets/banner/banner-group.png'
+
+const Banner = () => {
+    return (
+        <div className='bg-emerald-900 text-white h-[600px] gap-10 flex items-center justify-center px-20'>
+            <div className='w-1/3'>
+                <h2 className='text-4xl font-bold'>Your Best Medical Help Center</h2>
+                <p className='my-5'>Welcome to the premier online hub for healthcare professionals! Our doctors portal is your one-stop destination for all your medical needs. </p>
+                <button className="btn bg-orange-400 border-none text-white">All Services</button>
+            </div>
+
+            <div>
+                <img src={bannerGroup} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
